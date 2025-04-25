1d88d5b88fad0d177733182ee5fe273d7cabce0c
therealmanraj <singhmanraj8@gmail.com>
2025-04-25 15:33:57 -0700

    making the streamlit model

---
+++ streamlit/streamlit.py
@@ -0,0 +1,5 @@
+from components.data_ingestion import DataIngestion, DataIngestionConfig
+
+DataIngestionConfig = DataIngestionConfig()
+data_ingestion = DataIngestion()
+print("Data Ingestion module imported successfully.")
